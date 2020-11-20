8a1ed6144a14b289d901e377c2f4090e53e7d6a4
tbengtsen <tonebengtsen@gmail.com>
2020-11-20 16:31:08 +0100

    added clean_raw_pdbs.py

--- data/download_raw_pdbs.py
+++ data/download_raw_pdbs.py
@@ -14,21 +14,16 @@ target_path = os.path.dirname(target_directory)
 if not os.path.exists(target_path):
     os.makedirs(target_path)
 
+# download all pdbs from processed CATH4.3 from rcsb 
 for pdb in clean_data:
-    target_file = target_path + f'/{pdb}.pdb'
-    if not os.path.isfile(target_file):
-        print(pdb)
+    try:
+        response = urllib.request.urlopen(f'https://files.rcsb.org/download/{pdb}.pdb')
+        target_file = target_path + f'/{pdb}.pdb'
+        if not os.path.isfile(target_file):
+            with open(target_file, 'wb') as f:
+                f.write(response.read())
+        print(f'downloaded {pdb}')    
 
-
-#for pdb in clean_data:
-#    try:
-#        response = urllib.request.urlopen(f'https://files.rcsb.org/download/{pdb}.pdb')
-#        target_file = target_path + f'/{pdb}.pdb'
-#        if not os.path.isfile(target_file):
-#            with open(target_file, 'wb') as f:
-#                f.write(response.read())
-#        print(f'downloaded {pdb}')    
-#
-#    except: 
-#        print(f'cannot download {pdb}')
-#        
+    except: 
+        print(f'cannot download {pdb}')
+        
