bf86514d1b1c7a3457a2e8d42514c5e83feec413
Tone Bengtsen <toneben@g-12-l0002.cm.cluster>
2020-11-20 14:27:29 +0100

    added reduce in scripts

--- data/download_raw_pdbs.py
+++ data/download_raw_pdbs.py
@@ -2,31 +2,33 @@ import sys, os,json
 import urllib.request      
 
 
-# # load all PDB ids from CATH4.3 that are possible to simulate
-# clean_data_path = 'CATH4.3/CATH_cleaned_data.json'
-# with open(clean_data_path,'r') as f:
-#     clean_data = json.load(f)
+# load all PDB ids from CATH4.3 that are possible to simulate
+clean_data_path = 'CATH4.3/CATH_cleaned_data.json'
+with open(clean_data_path,'r') as f:
+    clean_data = json.load(f)
 
 
-clean_data = ['2lzw']
 # define download directory
 target_directory = 'pdbs_raw/'
 target_path = os.path.dirname(target_directory)
 if not os.path.exists(target_path):
     os.makedirs(target_path)
 
-
-
-
 for pdb in clean_data:
-    try:
-        response = urllib.request.urlopen(f'https://files.rcsb.org/download/{pdb}.pdb')
-        target_file = target_path + f'/{pdb}.pdb'
-        if not os.path.isfile(target_file):
-            with open(target_file, 'wb') as f:
-                f.write(response.read())
-            print(f'downloaded {pdb}')    
+    target_file = target_path + f'/{pdb}.pdb'
+    if not os.path.isfile(target_file):
+        print(pdb)
 
-    except: 
-        print(f'cannot download {pdb}')
-        
+
+#for pdb in clean_data:
+#    try:
+#        response = urllib.request.urlopen(f'https://files.rcsb.org/download/{pdb}.pdb')
+#        target_file = target_path + f'/{pdb}.pdb'
+#        if not os.path.isfile(target_file):
+#            with open(target_file, 'wb') as f:
+#                f.write(response.read())
+#        print(f'downloaded {pdb}')    
+#
+#    except: 
+#        print(f'cannot download {pdb}')
+#        
