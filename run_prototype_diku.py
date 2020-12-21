44a2e5ab3a2b317a9b6b8bb06fc0f6cd9c1d6426
tbengtsen <tonebengtsen@gmail.com>
2020-12-21 09:25:41 +0100

    run_proto diku

--- run_prototype_diku.py
+++ run_prototype_diku.py
@@ -17,10 +17,10 @@ cleaned_pdbs = glob.glob(path_cleaned +'/*.pdb')
 traj_format = 'xtc'
 
 # test run 5 proteins
-for path_pdb in cleaned_pdbs[:5]:
+for path_pdb in cleaned_pdbs[70:150]:
     name_pdb = path_pdb.split('/')[-1].split('_')[0]
     out_dir = f'{cwd}/simulations/{name_pdb}/'
-    cmd = f'{MD_python} {cwd}/protocol_prototype.py --pdb {cwd}/{path_pdb} --trajectory_format {traj_format} -o_dir {out_dir}'
+    cmd = f'{MD_python} {cwd}/protocol_prototype.py --pdb {cwd}/{path_pdb} --trajectory_format {traj_format} -o_dir {out_dir} --cuda'
     print('\ncmd:\n',cmd)
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
